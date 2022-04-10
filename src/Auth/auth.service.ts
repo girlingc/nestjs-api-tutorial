a88742683171f297d3643341c5c784190245caef
Christian Girling <girling.christian@gmail.com>
2022-04-09 21:58:00 -0700

    Custom error if user signs up with duplicate login

--- src/Auth/auth.service.ts
+++ src/Auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
@@ -11,16 +12,24 @@ export class AuthService {
     // generate the password hash
     const hash = await argon.hash(dto.password);
     // save the new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-    delete user.hash;
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
 
-    // return the saved user
-    return user;
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+    }
   }
 
   signin() {
