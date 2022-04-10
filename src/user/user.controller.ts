ceeb62a1aabe248cae72a03d0d0de0dc005b4364
Christian Girling <girling.christian@gmail.com>
2022-04-10 15:18:20 -0700

    Refactor controller to use custom decorators

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/Auth/decorator';
 import { JwtGuard } from 'src/Auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    console.log({
-      user: req.user,
-    });
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+  @Patch()
+  editUser() {
+    
   }
 }
